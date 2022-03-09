531942cdbadd3bb8ea230172ad8d28be4b9be325
Hachim Youssoufa <feihachim@yahoo.fr>
2022-03-09 09:34:17 +0100

    homepage

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Zeroblog');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+
+        yield MenuItem::section('Blog');
+        yield MenuItem::section('Utilisateurs');
+        yield MenuItem::section('Retour vers le site');
+        yield MenuItem::linkToRoute('Retour vers le site', 'fas fa-door-open', 'app_home');
+        //yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+}
--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -6,15 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class PostController extends AbstractController
 {
     /**
-     * @Route("/", name="app_home")
+     * @Route("/post", name="app_post")
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('post/index.html.twig', [
+            'controller_name' => 'PostController',
         ]);
     }
 }
